5b7a1a786358e2408d0f5373d77588c0a1b81732
Jarrod <jparks@g.harvard.edu>
2024-11-28 00:53:26 -0500

    Updated ip addresses and ports for docker-compose file, successfully created engagment table

--- data_ingestion/cassandra_setup.py
+++ data_ingestion/cassandra_setup.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 import data_ingestion.utils.logger as logger
 
-logger.get_logger("cassandra_setup")
+logger = logger.get_logger("cassandra_setup")
 
 def setup_cassandra_schema():
     cluster = Cluster(['127.0.0.1'])
@@ -17,19 +17,20 @@ def setup_cassandra_schema():
         logger.error(f"ERROR: Failed to set up cassandra cluster: {e}")
     
     try:
-        session.execute(""""
-            CREATE TABLE IF NOT EXISTS engagment(
+        session.execute("""
+            CREATE TABLE IF NOT EXISTS engagment (
                 stock_ticker TEXT,
                 timestamp TIMESTAMP,
                 total_comments int,
                 total_upvotes int,
                 avg_sentiment float, 
-                PRIMARY KEY((stock_ticker), timestamp, total_comments, total_upvotes))
-            ) WITH CLUSTERING ORDER BY (timestamp DESC, total_comments DESC, total_upvotes DESC)
-        """)
+                PRIMARY KEY ((stock_ticker), timestamp, total_comments, total_upvotes)
+            ) WITH CLUSTERING ORDER BY (timestamp DESC, total_comments DESC, total_upvotes DESC);
+        """)     
         logger.info(f"Successfully created engagment table")
     except Exception as e:
         logger.error(f"ERROR: failed to create table: {e}")
+        print(f"ERROR: failed to create table: {e}")
     
     session.shutdown()
     cluster.shutdown()
